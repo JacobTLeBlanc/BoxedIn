5259854c295b7e01f663404bfeecf8f64b3a7653
JacobTLeBlanc <leblancthomasjacob@gmail.com>
2019-06-20 11:07:40 -0300

    added score counter and coin asset

---
+++ Assets/Scripts/ballScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ballScript : MonoBehaviour
+{
+
+    // Init direction
+    private float initDirectionX;
+    private float initDirectionY;
+
+    // Current direction
+    private float currentX, currentY;
+
+    // Score
+    private int score;
+    public Text scoreText;
+
+    // Rigidbody
+    Rigidbody2D rb;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+
+        initDirectionX = 0.5f;
+        initDirectionY = 1.2f;
+
+        score = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = new Vector2(initDirectionX, initDirectionY);
+        currentX = initDirectionX;
+        currentY = initDirectionY;
+
+        SetScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D() {
+        score++;
+        SetScoreText();
+    }
+
+    void SetScoreText() {
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
---
+++ Assets/Scripts/borderScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class borderScript : MonoBehaviour
+{
+
+    // Speed of rotation
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            speed = -speed;
+        }
+
+        transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
